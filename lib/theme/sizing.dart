78be6e6444ffd0d4d0311cbcb6ad89fe0253440f
Makosai <kristopher.ali.dev@gmail.com>
2021-08-19 07:50:03 -0400

    Create a sizing scheme to reuse spacings and sizes throughout the project.

---
+++ lib/theme/sizing.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Sizing {
+  // Paddings
+  static const padding1 = 42.0;
+  static const padding2 = 20.0;
+  static const padding3 = 16.0;
+  static const padding4 = 14.0;
+  static const padding5 = 10.0;
+
+  // Images
+  static const icon1 = 18.0;
+}
