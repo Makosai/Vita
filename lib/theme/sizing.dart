e0981362730e6468271868852875c99e0af419d4
Makosai <kristopher.ali.dev@gmail.com>
2021-08-22 01:52:19 -0400

    Lower Sizing.padding1.

--- lib/theme/sizing.dart
+++ lib/theme/sizing.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/material.dart';
-
 class Sizing {
   // Paddings
-  static const padding1 = 42.0;
+  static const padding1 = 40.0;
   static const padding2 = 20.0;
   static const padding3 = 16.0;
   static const padding4 = 14.0;
