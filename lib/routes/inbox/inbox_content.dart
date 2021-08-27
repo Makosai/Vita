54badf21cf459c2e68ac1c971e2017638dd2548a
Makosai <kristopher.ali.dev@gmail.com>
2021-08-26 23:55:38 -0400

    Create a temporary Inbox widget.

---
+++ lib/routes/inbox/inbox_content.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:vita/theme/sizing.dart';
+import 'package:vita/theme/themes.dart';
+
+class InboxContent extends StatelessWidget {
+  InboxContent({required this.getInbox});
+
+  final List<Widget> Function() getInbox;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: CurrentTheme.content,
+        alignment: Alignment.topLeft,
+        child: Padding(
+            padding: EdgeInsets.all(Sizing.padding3),
+            child: ListView(
+              children: getInbox(),
+              addAutomaticKeepAlives: false,
+              cacheExtent: 100.0,
+            )),
+      ),
+    );
+  }
+}
