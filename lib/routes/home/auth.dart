47834f52eb7db9aeb17b8a8c3f89261e65ea792c
Makosai <kristopher.ali.dev@gmail.com>
2021-08-26 23:56:41 -0400

    Update the HomePage widget and add a placeholder Auth widget.

---
+++ lib/routes/home/auth.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Auth extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("TODO");
+  }
+
+}
---
+++ lib/routes/home/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:vita/theme/globals.dart';
+import 'package:vita/widget/content.dart';
+import 'package:vita/widget/sidebar.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title = Globals.projectName;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [Sidebar(), Auth()],
+        ),
+      ),
+    );
+  }
+}
