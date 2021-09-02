26a18dbb71511e132f81776ef4e6d4113dbb9522
Makosai <kristopher.ali.dev@gmail.com>
2021-09-02 14:58:11 -0400

    Setup a template for Google configs.

---
+++ lib/configs/config_auth_google.dart
@@ -0,0 +1,7 @@
+class GoogleInfo {
+  static const String DESKTOP_CLIENT_ID = "";
+  static const String DESKTOP_CLIENT_SECRET = "";
+
+  static const String ANDROID_CLIENT_ID = "";
+  static const String ANDROID_CLIENT_SECRET = "";
+}
