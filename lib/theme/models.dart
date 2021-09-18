fc37e9b35e6a684809b463ff1642c8bc6086c799
Makosai <kristopher.ali.dev@gmail.com>
2021-09-18 06:39:35 -0400

    Update AccountsModel.

--- lib/theme/models.dart
+++ lib/theme/models.dart
@@ -20,6 +20,31 @@ class SidebarModel with ChangeNotifier, DiagnosticableTreeMixin {
   }
 }
 
-class AccountModel with ChangeNotifier {
+class AccountsModel with ChangeNotifier, DiagnosticableTreeMixin {
+  int _index = -1;
+  Widget? _view;
 
+  int get index => _index;
+  Widget? get view => _view;
+
+
+
+  void setIndex(int newIndex) {
+    _index = newIndex;
+
+    notifyListeners();
+  }
+
+  void setView(Widget widget) {
+    _view = widget;
+
+    notifyListeners();
+  }
+
+  /// Makes `Counter` readable inside the devtools by listing all of its properties
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(IntProperty('index', index));
+  }
 }
